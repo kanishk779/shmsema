999ef740e5272c1cb1cbdff90bc1c510afcd80f7
kanishk779 <kashyapamankumar54@gmail.com>
2021-01-10 17:34:03 +0530

    Add write.c

---
+++ write.c
@@ -0,0 +1,23 @@
+#include "helper.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char ** argv){
+    // create the shared memory
+    if(argc != 2){
+        printf("usage - %s [write something]\n", argv[0]);
+        exit(0);
+    }
+    char *mem = attach_memory(filename, size);
+    if(mem == NULL){
+        printf("failed to get the shared memory (write.c)\n");
+        exit(0);
+    }
+    printf("Writing %s : \n", argv[1]);
+    strncpy(mem, argv[1], size);
+
+    detach_memory(mem);
+    
+    return 0;
+}
