5cdc9c9d1b9cb460322bfc4bb8d6aa474c84fa80
kanishk779 <kashyapamankumar54@gmail.com>
2021-01-10 17:36:26 +0530

    Add read.c

---
+++ read.c
@@ -0,0 +1,22 @@
+#include "helper.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char ** argv){
+    // create the shared memory
+    if(argc != 1){
+        printf("usage - %s no args\n", argv[0]);
+        exit(0);
+    }
+    char *mem = attach_memory(filename, size);
+    if(mem == NULL){
+        printf("failed to get the shared memory (write.c)\n");
+        exit(0);
+    }
+    printf("Reading %s : \n", mem);
+    // detach after reading
+    detach_memory(mem);
+    
+    return 0;
+}
